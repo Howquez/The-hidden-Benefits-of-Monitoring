39e1bde988a399c4e02f26bb1204f6cf7e832c2d
Hauke Roggenkamp <Hauke2Roggenkamp@gmail.com>
2017-10-14 15:28:25 +0200

    Delete Decision Trees etc.

--- R_Scripts/03_Data_Analysis.R
+++ R_Scripts/03_Data_Analysis.R
@@ -106,50 +106,6 @@ matrix(c("HiBad", "LOBad", "HiGood", "LOGood"), nrow =2)
 fisher.test((fishersData[1:2,1:2]))
 
 
-# decision trees ----------------------------------------------------------
-# 
-# 
-# #setting the tree control parameters
-# fitControl <- trainControl(method = "cv", number = 5)
-# cartGrid <- expand.grid(.cp=(1:50)*0.01)
-# 
-# #decision tree
-# tree_model <- train(perfDifference ~ IT + relProductivity + Yaphat, 
-#                     data = experimentData, method = "rpart", trControl = fitControl, tuneGrid = cartGrid)
-# print(tree_model)
-# 
-# #build tree with 0.01 as complexity parameter
-# main_tree <- rpart(perfDifference ~ IT + relProductivity + Yaphat, 
-#                    data = experimentData, control = rpart.control(cp=0.01))
-# prp(main_tree)
-# 
-# #conditional partitioning
-# tree_fit <- ctree(perfDifference ~ IT + relProductivity + Yaphat, 
-#                   data = experimentData)
-# plot(tree_fit, type="simple")
-# 
-# #check corresponding RMSE
-# pre_score <- predict(main_tree, type = "vector")
-# rmse(experimentData$perfDifference, pre_score)
-# 
-# #random forest
-# #set tuning parameters
-# control <- trainControl(method = "cv", number = 5)
-# 
-# #random forest model
-# rf_model <- train(perfDifference ~ IT + relProductivity + Yaphat, 
-#                   data = experimentData, method = "parRF",
-#                   trControl = control, prox = TRUE, allowParallel = TRUE)
-# 
-# #check optimal parameters
-# print(rf_model)
-# 
-# #random forest model with 1000 trees and mtry = 3 as it minimizes RMSE
-# forest_model <- randomForest(perfDifference ~ IT + relProductivity + Yaphat, 
-#                              data = experimentData, mtry = 3, ntree = 1000)
-# print(forest_model)
-# varImpPlot(forest_model)
-
 
 
 
