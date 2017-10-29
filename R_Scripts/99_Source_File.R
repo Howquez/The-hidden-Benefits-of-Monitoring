2dadbd74a85b886fcefe009d1b6e6c41bedd9756
Hauke Roggenkamp <Hauke2Roggenkamp@gmail.com>
2017-10-29 19:25:30 +0100

    Create 99_Source_File.R

---
+++ R_Scripts/99_Source_File.R
@@ -0,0 +1,9 @@
+rm(list=ls())
+setwd("/Users/howquez/Documents/002_UNI/UCPH/016_Master Thesis/05_Data")
+
+source("03_RCodes/01_Read_XML_Data.R")
+source("03_RCodes/02_Data_Manipulation.R")
+source("03_RCodes/03_Data_Analysis.R")
+source("03_RCodes/04_Graphs&Tables.R")
+source("03_RCodes/06_SubSample.R")
+source("03_RCodes/07_Shiny.R")
